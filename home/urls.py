1cc0564164f703006f1f857921b0ac34a6b557e2
Bibek <ghimirebibek@hotmail.com>
2020-10-13 19:23:41 +0545

    user authentication added

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from home import views
 
 urlpatterns = [
-   path('', views.login, name='login')
+   path('', views.login_view, name='login_view'),
+   path('signup/', views.signup_view, name='signup_view'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,14 +1,46 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
+
 def home(request):
     return HttpResponse('This is homepage.')
 
 def addproduct(request):
     return HttpResponse('This is the page to add products.')
 
-def login(request):
-    return HttpResponse('This is the login page.')
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            #log the user in
+            user = form.get_user()
+            login(request,user)
+            return render(request,'home.html')
 
-def signup(request):
-    return HttpResponse('This is the signup page.')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request,'login.html',{'form':form})
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            #log the user in
+            return render(request,'login.html')
+
+    else:
+        form = UserCreationForm
+
+    return render(request,'signup.html',{'form':form})
+    
+    
+    
+
+def contactus(request):
+    return HttpResponse('This is the Contact Us page.')
